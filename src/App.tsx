230399240f5acf51bcb05e76c9a216feae0299fe
xing142857 <50036214+xing142857@users.noreply.github.com>
2025-04-02 22:05:34 +1100

    Add timer text

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="w-screen h-screen flex items-center justify-center">
+        <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+          00:00:00
+        </p>
+        {/* <div className="w-1/4 aspect-square bg-red-500"></div>
+        <div className="w-1/6"></div>
+        <div className="w-1/4 aspect-square bg-red-500"></div>
+        <div className="w-1/6"></div>
+        <div className="w-1/4 aspect-square bg-red-500"></div> */}
+      </div>
     </>
   )
 }
