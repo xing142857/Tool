6f8a3b419a4a97e222efab50a53798516f57ebdf
xing142857 <50036214+xing142857@users.noreply.github.com>
2025-04-03 16:59:37 +1100

    Delete useless code

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,35 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [isRervesing, setIsReversing] = useState(false);
+
+  const startRunning = () => {
+    
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCount((prev) => prev + 1);
+      isRunning ? setCount((prev) => prev + 1) : null;
     }, 1000);
     return () => clearInterval(interval);
   }, [isRunning]);
 
   return (
     <div className="grid w-screen h-screen content-center">
-      <p className="flex items-center justify-center text-6xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-mono">
+      <p className="flex items-center justify-center text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-mono">
         00:00:00{count}
       </p>
       <div className='flex items-center justify-center h-24'></div>
-      <div className='flex items-center justify-center text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>
-        <button>
+      <div className='flex items-center justify-center text-3xl md:text-4xl lg:text-5xl xl:text-6xl gap-x-8'>
+        <button onClick={()=>setIsRunning(true)}>
           Start
         </button>
-        <div className="w-1/6"></div>
-        <button>
+        <button onClick={()=>setIsRunning(false)}>
           Stop
         </button>
-        <div className="w-1/6"></div>
-        <button>
+        <button onClick={()=>setIsReversing(true)}>
           Reverse
         </button>
       </div>
