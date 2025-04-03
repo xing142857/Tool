b77ef26affbf12578c6ea7a5f922d4c719ecab3d
xing142857 <50036214+xing142857@users.noreply.github.com>
2025-04-03 15:25:19 +1100

    Add button UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isRunning, setIsRunning] = useState(false);
 
-  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isRunning]);
 
   return (
-    <>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
-          00:00:00
-        </p>
-        {/* <div className="w-1/4 aspect-square bg-red-500"></div>
+    <div className="grid w-screen h-screen content-center">
+      <p className="flex items-center justify-center text-6xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-mono">
+        00:00:00{count}
+      </p>
+      <div className='flex items-center justify-center h-24'></div>
+      <div className='flex items-center justify-center text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>
+        <button>
+          Start
+        </button>
         <div className="w-1/6"></div>
-        <div className="w-1/4 aspect-square bg-red-500"></div>
+        <button>
+          Stop
+        </button>
         <div className="w-1/6"></div>
-        <div className="w-1/4 aspect-square bg-red-500"></div> */}
+        <button>
+          Reverse
+        </button>
       </div>
-    </>
+    </div>
   )
 }
 
